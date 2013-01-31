fdddd58ade9efa9d64adbda5d707e82f1d317997
Splizard <quentin@squeezedoranges.com>
2013-01-31 17:16:18 +1300

    Make spawning static.

--- mods/spawning/init.lua
+++ mods/spawning/init.lua
@@ -88,32 +88,8 @@ end
 function spawning.spawn(player, place)
 	local spawn = registered_spawns[place]
 	if spawn then
-		--if spawn.mode == "static" then
-		   	local pos = spawn.pos
-		   	local env = minetest.env
-		   	if pos then
-				if env:get_node({x=pos.x,y=pos.y+1,z=pos.z}).name ~= "air" then
-					for y=pos.y, pos.y+30 do
-						local node = env:get_node({x=pos.x,y=y,z=pos.z})
-						if node.name == "ignore" then
-							player:setpos({x=pos.x,y=pos.y+y+1,z=pos.z})
-							minetest.after(2, spawning.spawn, player, place)
-							return true
-						end
-						if node.name == "air" then
-							player:setpos({x=pos.x,y=pos.y+y+1,z=pos.z})
-							return true
-						elseif node.name == "default:water_source" then
-							player:setpos({x=pos.x,y=pos.y+y+1,z=pos.z})
-							return true
-						end
-					end
-					player:setpos({x=pos.x,y=pos.y+25,z=pos.z})
-				end
-				player:setpos(pos)
-				return true
-			end
-		--end
+	   	local pos = spawn.pos
+		player:setpos(pos)
 	end
 end
 
