3253423dc69ed5c2d75b17d4fe09d25f143a53ba
Splizard <quentin@squeezedoranges.com>
2013-02-21 12:39:25 +1300

    Fixed bug. bad argument #1 to 'get_node' (table expected, got nil)

--- mods/random_chests/init.lua
+++ mods/random_chests/init.lua
@@ -117,10 +117,10 @@ function random_chests.refill(i)
 		if chests[i] then
 			local n = env:get_node(chests[i]).name
 			if (not n:match("default:chest")) and n ~= "ignore" then
-				table.remove(chests,i)
 				print("chest missing! found:")
 				print(env:get_node(chests[i]).name)
 				print("instead")
+				table.remove(chests,i)
 			else
 				fill_chest(chests[i])
 			end
